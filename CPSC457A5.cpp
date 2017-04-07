d977ddaee2af6c0d31c561f36aea6f24ccf7c204
abuiza <abuiza@zone06-ec.cs.ucalgary.ca>
2017-04-07 17:25:44 -0600

    more updates

--- CPSC457A5.cpp
+++ CPSC457A5.cpp
@@ -3,94 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_QUEUE_SIZE 20
-// QUEUE
-typedef struct queue{
-	int element[MAX_QUEUE_SIZE];
-	uint8_t head;
-	uint8_t tail;
-	uint8_t remaining_elements;
-} prod_cons_queue;
 
-pthread_mutex_t lock;
-pthread_cond_t notfull;
-pthread_cond_t empty;
-
-void queue_initialize(prod_cons_queue *q)
-{
-	q.head = NULL;
-	q.tail = NULL;
-	q.remaining_elements = 0;
-}
-
-void queue_add(prod_cons_queue *q, int elem)
-{
-	while(queue_full(q))
-	{
-		while(!notfull)
-		{
-			pthread_cond_wait(&notfull, &mutex);
-		}
-		pthread_mutex_lock(&lock);
-		if(queue_full(q))
-		{
-			pthread_mutex_unlock(&lock);
-		}
-	}
-	if(head == NULL || tail == NULL)
-	{
-		q.head = 0;
-		q.tail = 0;
-		pthread_cond_signal(&empty);
-	}
-	q.tail++;
-	if(q.tail >= MAX_QUEUE_SIZE)
-	{
-		q.tail = 0;
-	}
-	q.element[q.tail] = elem;
-	q.remaining_elements++;
-	pthread_mutex_unlock(&lock);
-}
-
-int queue_remove(prod_cons_queue *q)
-{
-	pthread_mutex_lock(&lock);
-	int prodMsg;
-	prodMsg = q.element[q.head];
-	q.remaining_elements--;
-	q.head++;
-	if(q.head >= MAX_QUEUE_SIZE)
-	{
-		q.head = 0;
-	}
-	if(q.remaining_elements <= 0)
-	{
-		q.head = NULL;
-		q.tail = NULL;
-	}
-	pthread_mutex_unlock(&lock);
-	return prodMsg;
-}
-
-boolean queue_full(prod_cons_queue *q)
-{
-	if(remaining_elements >= MAX_QUEUE_SIZE)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-// CONSUMER
+// CONSUMER routine
 void *consumer(void *q){
+	//cache the queue object
+
+
+
 	int msgCount = 0;
 	int msgOut;
 	while(msgCount < 100)
 	{
+
+		//try to lock
+
+		//is the queue empty?
+			//empty - pthread_cond_wait
+			//not empty
+				//get an element from the queue
+				//was the queue full before?
+					//yes - signal the producer
+					// no - keep going 
+
+		//unlock
+
+
 		if(q.remaining_elements <= 0)
 		{
 			while(!empty)
@@ -105,17 +42,49 @@ void *consumer(void *q){
 	pthread_exit(NULL);
 }
 
-// PRODUCER
+// PRODUCER routine
 void *producer(void *threadId)
 {
 	int tid;
 	tid = (int)threadId;
+
+	//cache the queue object
+
+
+	//try to lock
+
+	//is the queue full?
+		//full- pthread_cond_wait
+
+		//not full
+			//add the id to the queue
+			//was it empty before?
+				//yes - signal the consumer
+				//no - keep going
+
+	//unlock
+
 }
 
 // MAIN
 int main()
 {
+
+	pthread_mutex_t lock;
+	pthread_cond_t notfull;
+	pthread_cond_t empty;
+
+	pthread_t Producer_Threads[10];
+	pthread_t Consumer;
+
+
+//Create the Queue object
+
+//Create the threads and pass the Queue object and id to them
+
 	pthread_mutex_init(&lock,NULL);
+
+
 
 	pthread_mutex_destroy(&mutex);
 }
---
+++ Queue.h
@@ -0,0 +1,26 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define MAX_QUEUE_SIZE 20
+
+class Queue{
+  private:
+    // QUEUE
+    struct queue{
+    	int element[MAX_QUEUE_SIZE];
+    	uint8_t head;
+    	uint8_t tail;
+    	uint8_t remaining_elements;
+    } q;
+
+  public:
+    Queue();
+
+    queue_initialize();
+    queue_add(int elem);
+    queue_remove();
+    queue_full();
+}
+
+
+#endif
