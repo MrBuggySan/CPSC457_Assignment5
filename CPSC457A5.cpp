262f8be02efe04e7731b50ed9bf8267a9d1d20af
abuiza <abuiza@zone47-eb.cs.ucalgary.ca>
2017-04-09 13:24:37 -0600

    working to pass variables to the threads

--- CPSC457A5.cpp
+++ CPSC457A5.cpp
@@ -2,7 +2,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "Queue.h"
 
+#define NUM_PRODUCER 10
+#define NUM_CONSUMER 1
+
+struct thread_data
+{
+   int	thread_id;
+   Queue* q;
+};
 
 // CONSUMER routine
 void *consumer(void *q){
@@ -23,7 +32,7 @@ void *consumer(void *q){
 				//get an element from the queue
 				//was the queue full before?
 					//yes - signal the producer
-					// no - keep going 
+					// no - keep going
 
 		//unlock
 
@@ -69,22 +78,63 @@ void *producer(void *threadId)
 // MAIN
 int main()
 {
-
+	int rc;
 	pthread_mutex_t lock;
+
+	//?
 	pthread_cond_t notfull;
 	pthread_cond_t empty;
 
-	pthread_t Producer_Threads[10];
+	pthread_t Producer_Threads[NUM_PRODUCER];
 	pthread_t Consumer;
-
-
-//Create the Queue object
-
-//Create the threads and pass the Queue object and id to them
 
 	pthread_mutex_init(&lock,NULL);
 
+//Create the struct that contains Queue object and id
+	struct thread_data thread_data_array[NUM_PRODUCER + NUM_CONSUMER];
 
+//Create a single Queue object
+	Queue* queue = new Queue();
+
+
+//Create the producer threads
+	for(int i = 0; i < NUM_PRODUCER; i++){
+		thread_data_array[i].thread_id = t;
+		thread_data_array[i].q = queue;
+		printf("Creating producer #%d \n", i);
+		rc = pthread_create(&Producer_Threads[t], NULL, producer, (void *)
+	       &thread_data_array[t]);
+	  if (rc) {
+	    printf("ERROR; return code from producer pthread_create() is %d\n", rc);
+	    exit(-1);
+	    }
+	  }
+	}
+
+//Create the consumer threads
+	int consum_start_index = NUM_PRODUCER;
+	int consum_end_index = NUM_PRODUCER + NUM_CONSUMER;
+	for(int i = consum_start_index; i < consum_end_index; i++){
+		thread_data_array[i].thread_id = t;
+		thread_data_array[i].q = queue;
+		printf("Creating consumer #%d \n", i);
+		rc = pthread_create(&Producer_Threads[t], NULL, producer, (void *)
+	       &thread_data_array[t]);
+	  if (rc) {
+	    printf("ERROR; return code from consumer pthread_create() is %d\n", rc);
+	    exit(-1);
+	    }
+	  }
+
+	}
+
+
+
+
+	//Wait for all of the other threads to join
+
+	//delete pointers
+	delete queue;
 
 	pthread_mutex_destroy(&mutex);
 }
